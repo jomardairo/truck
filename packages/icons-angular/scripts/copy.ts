6c276ac1dc8b8fce0005060821967985cb3fe66c
jomardairo <jomardairo@yahoo.com>
2025-01-02 10:46:35 +0000

    Optimize configuration - refactoring code

--- packages/icons-angular/scripts/copy.ts
+++ packages/icons-angular/scripts/copy.ts
@@ -1,9 +1,9 @@
-const fs = require('fs-extra');
-const path = require('path');
+let fs = require('fs-extra');
+let path = require('path');
 
 function copyAssets() {
-    const from = path.resolve(__dirname, '../src/inline-svg');
-    const to = path.resolve(__dirname, '../dist/icons/src/inline-svg');
+    let from = path.resolve(__dirname, '../src/inline-svg');
+    let to = path.resolve(__dirname, '../dist/icons/src/inline-svg');
     fs.copy(from, to);
 }
 
