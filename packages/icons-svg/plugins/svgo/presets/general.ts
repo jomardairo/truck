89f0bffea90c0f91d38adbeacb2fd50e362a3602
jomardairo <jomardairo@yahoo.com>
2025-01-06 13:07:32 +0000

    Refactor dependencies - fixing typo

--- packages/icons-svg/plugins/svgo/presets/general.ts
+++ packages/icons-svg/plugins/svgo/presets/general.ts
@@ -2,7 +2,7 @@ import SVGO from 'svgo';
 import { mergeRight } from 'ramda';
 import { base } from './base';
 
-export let generalConfig: SVGO.Options = mergeRight(base, {
+export const generalConfig: SVGO.Options = mergeRight(base, {
   plugins: [
     ...(base.plugins || []),
     { removeAttrs: { attrs: ['class', 'fill'] } }
