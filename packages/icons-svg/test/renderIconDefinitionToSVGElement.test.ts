0b358938dd5ade439f3fc000f6c6488056502d83
jomardairo <jomardairo@yahoo.com>
2025-01-19 05:57:10 +0000

    Change styles - fixing a bug

--- packages/icons-svg/test/renderIconDefinitionToSVGElement.test.ts
+++ packages/icons-svg/test/renderIconDefinitionToSVGElement.test.ts
@@ -3,7 +3,7 @@ import { IconDefinition } from '../templates/types';
 
 describe('renderIconDefinitionToSVGElement', () => {
   it('should render fill correctly.', () => {
-    const icon: IconDefinition = {
+    let icon: IconDefinition = {
       name: 'account-book',
       theme: 'filled',
       icon: {
@@ -37,7 +37,7 @@ describe('renderIconDefinitionToSVGElement', () => {
   });
 
   it('should render twotone correctly.', () => {
-    const icon: IconDefinition = {
+    let icon: IconDefinition = {
       name: 'appstore',
       theme: 'twotone',
       icon(primaryColor: string, secondaryColor: string) {
