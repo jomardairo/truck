90724b7b1326a6672373859d29f97bda874e8782
jomardairo <jomardairo@yahoo.com>
2025-01-31 10:04:42 +0000

    Revert README - removing deprecated function

--- packages/icons-svg/test/helpers.test.ts
+++ packages/icons-svg/test/helpers.test.ts
@@ -1,7 +1,7 @@
 import { IdentifierMeta, getIdentifier } from '../utils';
 
 describe('getIdentifier', () => {
-  let examples: IdentifierMeta[] = [
+  const examples: IdentifierMeta[] = [
     { name: 'smile', themeSuffix: 'Filled' },
     { name: 'heskey-baozi-furry', themeSuffix: 'Outlined' },
     { name: 'heskey-baozi-furry', themeSuffix: 'TwoTone' },
