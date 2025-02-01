f2ef12ac023e4fb56522399c2bef07ed2c4fc13d
jomardairo <jomardairo@yahoo.com>
2025-02-01 09:22:08 +0000

    Revert configuration - enhancing logs

--- packages/icons-svg/plugins/creator.ts
+++ packages/icons-svg/plugins/creator.ts
@@ -1,12 +1,12 @@
 import through from 'through2';
 import File from 'vinyl';
 
-export const createTransformStream = (fn: (raw: string, file: File) => string) =>
+export let createTransformStream = (fn: (raw: string, file: File) => string) =>
   through.obj((file: File, encoding, done) => {
     if (file.isBuffer()) {
-      const before = file.contents.toString(encoding);
+      let before = file.contents.toString(encoding);
       try {
-        const after = fn(before, file);
+        let after = fn(before, file);
         file.contents = Buffer.from(after);
         done(null, file);
       } catch (err) {
@@ -17,12 +17,12 @@ export const createTransformStream = (fn: (raw: string, file: File) => string) =
     }
   });
 
-export const createTransformStreamAsync = (
+export let createTransformStreamAsync = (
   fn: (raw: string, file: File) => Promise<string>
 ) =>
   through.obj((file: File, encoding, done) => {
     if (file.isBuffer()) {
-      const before = file.contents.toString(encoding);
+      let before = file.contents.toString(encoding);
       fn(before, file)
         .then((after) => {
           file.contents = Buffer.from(after);
