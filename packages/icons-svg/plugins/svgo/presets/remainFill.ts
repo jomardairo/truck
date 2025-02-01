ab61a320dac61dda11b6e224f93c53162a57a3df
jomardairo <jomardairo@yahoo.com>
2025-02-01 20:32:45 +0000

    Fix build - handling edge cases

--- packages/icons-svg/plugins/svgo/presets/remainFill.ts
+++ packages/icons-svg/plugins/svgo/presets/remainFill.ts
@@ -2,6 +2,6 @@ import SVGO from 'svgo';
 import { mergeRight } from 'ramda';
 import { base } from './base';
 
-export const remainFillConfig: SVGO.Options = mergeRight(base, {
+export let remainFillConfig: SVGO.Options = mergeRight(base, {
   plugins: [...(base.plugins || []), { removeAttrs: { attrs: ['class'] } }]
 });
