20748eb016b0e4a726b08e4119a4bf7c51266c23
jomardairo <jomardairo@yahoo.com>
2025-02-16 16:28:24 +0000

    Refactor styles - fixing a bug

--- packages/icons-react/src/index.ts
+++ packages/icons-react/src/index.ts
@@ -4,4 +4,4 @@ export * from './icons';
 export * from './components/twoTonePrimaryColor';
 export { default as createFromIconfontCN } from './components/IconFont';
 export { default } from './components/Icon';
-export let IconProvider = Context.Provider;
+export const IconProvider = Context.Provider;
