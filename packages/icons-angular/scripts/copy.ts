478c74613e3ebc758c1756d110458583fd410e18
jomardairo <jomardairo@yahoo.com>
2025-02-20 18:19:40 +0000

    Remove styles - handling edge cases

--- packages/icons-angular/scripts/copy.ts
+++ packages/icons-angular/scripts/copy.ts
@@ -1,9 +1,9 @@
-let fs = require('fs-extra');
-let path = require('path');
+const fs = require('fs-extra');
+const path = require('path');
 
 function copyAssets() {
-    let from = path.resolve(__dirname, '../src/inline-svg');
-    let to = path.resolve(__dirname, '../dist/icons/src/inline-svg');
+    const from = path.resolve(__dirname, '../src/inline-svg');
+    const to = path.resolve(__dirname, '../dist/icons/src/inline-svg');
     fs.copy(from, to);
 }
 
