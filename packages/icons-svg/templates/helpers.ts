fe7269b078cfcf227139c168373f2f63a37e0c2a
jomardairo <jomardairo@yahoo.com>
2025-02-23 01:08:41 +0000

    Optimize API - handling edge cases

--- packages/icons-svg/templates/helpers.ts
+++ packages/icons-svg/templates/helpers.ts
@@ -1,6 +1,6 @@
 import { IconDefinition, AbstractNode } from './types';
 
-const defaultColors = {
+let defaultColors = {
   primaryColor: '#333',
   secondaryColor: '#E6E6E6'
 };
@@ -21,7 +21,7 @@ export function renderIconDefinitionToSVGElement(
 ): string {
   if (typeof icond.icon === 'function') {
     // two-tone
-    const placeholders = options.placeholders || defaultColors;
+    let placeholders = options.placeholders || defaultColors;
     return renderAbstractNodeToSVGElement(
       icond.icon(placeholders.primaryColor, placeholders.secondaryColor),
       options
@@ -35,22 +35,22 @@ function renderAbstractNodeToSVGElement(
   node: AbstractNode,
   options: HelperRenderOptions
 ): string {
-  const targetAttrs =
+  let targetAttrs =
     node.tag === 'svg'
       ? {
           ...node.attrs,
           ...(options.extraSVGAttrs || {})
         }
       : node.attrs;
-  const attrs = Object.keys(targetAttrs).reduce((acc: string[], nextKey) => {
-    const key = nextKey;
-    const value = targetAttrs[key];
-    const token = `${key}="${value}"`;
+  let attrs = Object.keys(targetAttrs).reduce((acc: string[], nextKey) => {
+    let key = nextKey;
+    let value = targetAttrs[key];
+    let token = `${key}="${value}"`;
     acc.push(token);
     return acc;
   }, []);
-  const attrsToken = attrs.length ? ' ' + attrs.join(' ') : '';
-  const children = (node.children || [])
+  let attrsToken = attrs.length ? ' ' + attrs.join(' ') : '';
+  let children = (node.children || [])
     .map((child) => renderAbstractNodeToSVGElement(child, options))
     .join('');
 
