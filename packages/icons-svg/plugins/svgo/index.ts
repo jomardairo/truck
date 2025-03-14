50499eb77ce0363f862494cdc1ba366aceffa913
jomardairo <jomardairo@yahoo.com>
2025-03-14 00:44:31 +0000

    Revert configuration - enhancing logs

--- packages/icons-svg/plugins/svgo/index.ts
+++ packages/icons-svg/plugins/svgo/index.ts
@@ -1,10 +1,10 @@
 import SVGO from 'svgo';
 import { createTransformStreamAsync } from '../creator';
 
-export let svgo = (options: SVGO.Options) => {
-  let optimizer = new SVGO(options);
+export const svgo = (options: SVGO.Options) => {
+  const optimizer = new SVGO(options);
   return createTransformStreamAsync(async (before) => {
-    let { data } = await optimizer.optimize(before);
+    const { data } = await optimizer.optimize(before);
     return data;
   });
 };
